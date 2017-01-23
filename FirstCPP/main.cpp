25daddc727837e96b6c24f5fc32bb2a751940c69
Jake Nilson <j.nilson132@gmail.com>
2017-01-23 12:36:54 -0700

    First C++ Project

---
+++ FirstCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstCPP
+//
+//  Created by Nilson, Jake on 1/23/17.
+//  Copyright © 2017 Nilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
