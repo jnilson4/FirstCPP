196842ebd65f7ae0d032ca8edb15a6e3c0fcf1cb
Jake Nilson <j.nilson132@gmail.com>
2017-01-31 13:34:07 -0700

    Added the methods for the timer.

--- FirstCPP/Model/Timer.cpp
+++ FirstCPP/Model/Timer.cpp
@@ -7,3 +7,37 @@
 //
 
 #include "Timer.hpp"
+#include <iostream>
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: finishTimer()
+{
+    executionTime = clock() - executionTime;
+}
+
+long Timer :: getExecutionTimeInMicroseconds()
+{
+    return executionTime;
+}
+
+void Timer :: displayTimerInformation()
+{
+    cout << "It took this long to execute: " << executionTime << endl;
+    cout << "That is this many seconds: " << executionTime/CLOCKS_PER_SEC << endl;
+}
